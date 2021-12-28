28a24588318484876e4e6aabe5bd67e7729e5c1a
Mark <56827081+MarkVoitov@users.noreply.github.com>
2021-12-28 18:58:46 +0300

    Update and rename 2. Cтандартные средства языка Python/task01.py to 2. Cтандартные средства языка Python/2.2 Работа с кодом: модули и импорт/task01.py

--- 2. Cтандартные средства языка Python/task01.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-date = [int(i) for i in input().split()]
-days = int(input())
-
-start_time = datetime.date(date[0], date[1], date[2])
-period = datetime.timedelta(days=days)
-
-print((start_time + period).strftime("%Y %m %d").replace(" 0", " "))
